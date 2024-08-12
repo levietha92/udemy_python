8dbf09dd257c76fc3e76dbdc7a019283fe932c0e
levietha92 <ha.lv@joonsolutions.com>
2024-08-12 22:04:12 +0700

    it work but too hard lol

--- d23/car_manager.py
+++ d23/car_manager.py
@@ -1,7 +1,27 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
+SCREEN_SIZE = 600
+HALF_SIZE = SCREEN_SIZE/2
 
-class CarManager:
-    pass
+
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.shapesize(1,2)
+        self.color(random.choice(COLORS))
+        car_x = random.randint(HALF_SIZE, HALF_SIZE + 20)
+        car_y = random.randint(-HALF_SIZE +30 , HALF_SIZE - 30)
+        self.penup()
+        self.goto(car_x,car_y)
+        self.setheading(180)
+        # self.car_list = []
+            
+
+    def move(self):
+        self.forward(MOVE_INCREMENT)
--- d23/main.py
+++ d23/main.py
@@ -1,14 +1,40 @@
 import time
 from turtle import Screen
 from player import Player
-from car_manager import CarManager
+from car_manager import *
 from scoreboard import Scoreboard
 
 screen = Screen()
+screen.bgcolor("black")
 screen.setup(width=600, height=600)
 screen.tracer(0)
+screen.listen()
+
+tim = Player()
+car_list = []
+
+
 
 game_is_on = True
 while game_is_on:
+    
     time.sleep(0.1)
     screen.update()
+    screen.onkey(fun=tim.go_forward, key="Up")
+    screen.onkey(fun=tim.go_backward, key="Down")
+
+    # print(time.clock_gettime)
+    i = CarManager()
+    car_list.append(i)
+
+    for car in car_list:
+        car.move()
+        if tim.distance(car) <= 10:
+            print("GAME OVER")
+            game_is_on = False
+            
+    if tim.reach_finish_line():
+        game_is_on = False
+    
+    
+    
