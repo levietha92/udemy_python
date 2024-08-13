5ea471ad0189a1a34dc27ae8db0672efb606c4cb
levietha92 <ha.lv@joonsolutions.com>
2024-08-13 18:45:11 +0700

    other ways

---
+++ d25/main_os.py
@@ -0,0 +1,5 @@
+import os
+
+with open("weather_data.csv") as file:
+    data = file.readlines()
+    print(data)
---
+++ d25/main_pandas.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+data = pd.read_csv("weather_data.csv")
+data.head()
