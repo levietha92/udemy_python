280c793df3861c8683851366faeb7415ab8c4f81
levietha92 <ha.lv@joonsolutions.com>
2024-08-13 17:33:49 +0700

    filepath understood

--- d24/filepath/main.py
+++ d24/filepath/main.py
@@ -1,7 +1,21 @@
 import os
 
 with open("./my_file.txt") as file:
+    # /Users/hanna/GitHub/udemy_course/d24/filepath/my_file.txt
     print(file.read())
 
 with open("/Users/hanna/Desktop/my_file.txt") as file:
-    print(file.read())
+    print(file.read()+ " with absolute path")
+
+# relative path
+with open("../../../../Downloads/my_file.txt") as file:
+    print(file.read() + " with relative path")
+
+with open("../../../../Desktop/my_file.txt") as file:
+    print(file.read() + " with relative path")
+"""
+Python: Everything is in forward slash in both Mac and Win
+Mac: forward slash, Wins: backslash
+../ = going up the folder structure by 1
+./ = going down the folder structure by 1
+"""    
