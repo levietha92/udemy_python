a502546d35ef6c54ec185bd3f2af26ad06a82e1b
levietha92 <ha.lv@joonsolutions.com>
2024-08-18 11:11:33 +0700

    tkinter tryout

--- d27/main.py
+++ d27/main.py
@@ -7,5 +7,20 @@ window.minsize(500,300)
 
 label = tkinter.Label(text="Yooohooo", font=("Arial", 30, "italic"))
 label.pack(side='top') # place on the screen, without this it doesnt show
+label.config(text="New text")
+
+# Button
+
+def button_clicked():
+    # print("I got cliked")
+    new_text = input.get()
+    label.config(text=new_text)
+
+button = tkinter.Button(text="Click me", command=button_clicked)
+button.pack() #without this it doesn't show
+
+# Entry
+input = tkinter.Entry(width=10)
+input.pack()
 
 window.mainloop()
