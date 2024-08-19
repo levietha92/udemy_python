858cddafa087ebbab1774dee472883de8159fc31
levietha92 <ha.lv@joonsolutions.com>
2024-08-19 16:23:02 +0700

    save pwd function worked

--- d29/password-manager-start/main.py
+++ d29/password-manager-start/main.py
@@ -1,9 +1,21 @@
 import tkinter
 import random
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
-
+def generate_pwd():
+    new_password = []
+    random.ran
 # ---------------------------- SAVE PASSWORD ------------------------------- #
-
+def add_password():
+    website = website_entry.get()
+    email = email_entry.get()
+    pwd = pwd_entry.get()
+    with open("data.txt", mode="a") as file:
+        row_insert = f"{website} | {email} |  {pwd} \n"
+        print(row_insert)
+        file.write(row_insert)
+        # then delete them
+        website_entry.delete(0,'end')
+        pwd_entry.delete(0,'end')
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = tkinter.Tk()
@@ -21,6 +33,7 @@ website_text.grid(column=0, row=1)
 
 website_entry = tkinter.Entry(width=35)
 website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()
 
 #Email
 email_text = tkinter.Label(text="Email/Username", width=15)
@@ -28,6 +41,7 @@ email_text.grid(column=0,row=2)
 
 email_entry = tkinter.Entry(width=35)
 email_entry.grid(column=1,row=2, columnspan=2)
+email_entry.insert(0,"mumbojumbo@gmail.com")
 
 #Password
 pwd_text = tkinter.Label(text="Password", width=15)
@@ -40,6 +54,10 @@ generate_button = tkinter.Button(text="Generate Pwd")
 generate_button.grid(column=2,row=3)
 
 #Add button
-add_button = tkinter.Button(text="Add",width=32)
+add_button = tkinter.Button(text="Add",width=32, bg="red", command=add_password)
 add_button.grid(column=1,row=4,columnspan=2)
+
+# FUNCTIONS: receive the input when user click ADD, save input to txt file
+
+
 window.mainloop()
