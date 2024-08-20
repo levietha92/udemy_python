94867d5929599585bc827e3641bce5985ce20cf8
levietha92 <ha.lv@joonsolutions.com>
2024-08-20 11:34:34 +0700

    add search feature ok

--- d30/password-manager/main.py
+++ d30/password-manager/main.py
@@ -10,12 +10,20 @@ def password_insert():
     pyperclip.copy(new_pwd) # this helps you copy into clipboard
     cfm_popup = messagebox.askyesno(title="Password generated", message=f"Your password is {new_pwd}")
     return cfm_popup
-    
+
 def password_confirm():
     if password_insert() != True:
         password_insert()
-    
 
+
+# ---------------------------- SEARCH PASSWORD ------------------------------- #
+def search():
+    website = website_entry.get()
+    # website = "test_no_json"
+    with open("data.json", mode="r") as file:
+        data = json.load(file)
+        print(data[website])
+        inform_popup = messagebox.showinfo(title=f"{website}", message=f"Email: {data[website]['email']} \n Password: {data[website]['password']}")
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def add_password():
     website = website_entry.get()
@@ -39,7 +47,7 @@ def add_password():
             file2 = open("data.json", mode="w")
             json.dump(data, file2, indent=4)
         finally:
-            file.close()    
+            file.close()
     # then delete them
     website_entry.delete(0,'end')
     pwd_entry.delete(0,'end')
@@ -58,8 +66,8 @@ canvas.grid(column=1, row=0)
 website_text = Label(text="Website", width=15)
 website_text.grid(column=0, row=1)
 
-website_entry = Entry(width=35)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(column=1, row=1, columnspan=1)
 website_entry.focus()
 
 #Email
@@ -77,16 +85,15 @@ pwd_text.grid(column=0,row=3)
 pwd_entry = Entry(width=21)
 pwd_entry.grid(column=1, row=3)
 
-generate_button = Button(text="Generate Pwd", command=password_confirm)
+generate_button = Button(text="Generate Pwd", width=10, command=password_confirm)
 generate_button.grid(column=2,row=3)
 
 #Add button
 add_button = Button(text="Add",width=32, bg="red", command=add_password)
 add_button.grid(column=1,row=4,columnspan=2)
 
-# FUNCTIONS: receive the input when user click ADD, save input to txt file
-
-
-
-
+# Search button
+search_button = Button(text="Search", width=10, command=search)
+search_button.grid(column=2, row=1)
+# FUNCTIONS: receive the input when user click ADD, save input to txt file, adding search function
 window.mainloop()
