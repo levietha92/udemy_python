b4f1bdda9184ccde96d75f428eb20d5d1722bad2
levietha92 <ha.lv@joonsolutions.com>
2024-08-20 10:17:55 +0700

    solved challenge and init nato

--- d30/main.py
+++ d30/main.py
@@ -35,6 +35,56 @@ else:
     print(content)
     print("This will only run if no exception block occurs")
 finally:
-    print("This will run no matter what happens above")
-    file.close()
-    print("File is closed, because we didnt' use with block")    
+    # print("This will run no matter what happens above")
+    # file.close()
+    # print("File is closed, because we didnt' use with block")
+    raise TypeError("an error i made up")
+
+# Try out
+height = float(input("Height: "))
+weight = int(input("Weight: "))
+
+if height > 3:
+    raise ValueError("Are you human?")
+
+fruits = ["Apple", "Pear", "Orange"]
+
+# Catch the exception and make sure the code runs without crashing.
+def make_pie(index):
+    try:
+        fruit = fruits[index]
+        print(fruit + " pie")
+    except:
+        if index > len(fruits):
+            print("Fruit pie")
+
+make_pie(4)
+
+
+# Catch the exception and make sure the code runs without crashing.
+facebook_posts = [
+    {'Likes': 21, 'Comments': 2},
+    {'Likes': 13, 'Comments': 2, 'Shares': 1},
+    {'Likes': 33, 'Comments': 8, 'Shares': 3},
+    {'Comments': 4, 'Shares': 2},
+    {'Comments': 1, 'Shares': 1},
+    {'Likes': 19, 'Comments': 3}
+]
+
+
+def count_likes(posts):
+    total_likes = 0
+    for post in posts:
+        try:
+            total_likes += post['Likes']
+        except KeyError:
+            total_likes += 0
+    return total_likes
+    print(total_likes)
+
+
+count_likes(facebook_posts)
+
+
+
+
