3600e4c4db92684d3f8a464ed0be58e6661b8860
levietha92 <ha.lv@joonsolutions.com>
2024-08-20 10:35:59 +0700

    improve nato game

--- d30/nato_alphabet/main.py
+++ d30/nato_alphabet/main.py
@@ -11,9 +11,20 @@ data_dict
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-user_input = input("What is your name?")
-user_input_list = [letter.upper() for letter in user_input]
+def ask_user():
+    user_input = input("What is your name?")
+    return user_input
 
+game_is_on = True
 
-nato = [data_dict[letter] for letter in user_input_list]    
-nato
+while game_is_on:
+    try:
+        user_input_list = [letter.upper() for letter in ask_user()]
+        nato = [data_dict[letter] for letter in user_input_list]
+        print(nato)
+    except:
+        print(f"That is not a letter. Try again")
+        ask_user()
+    finally:
+        if ask_user() == "":
+            game_is_on = False
