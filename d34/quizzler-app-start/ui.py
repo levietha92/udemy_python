37a783a360b0a5e0613a9f0f4e806630f6723286
levietha92 <ha.lv@joonsolutions.com>
2024-08-23 09:08:42 +0700

    done UI

--- d34/quizzler-app-start/ui.py
+++ d34/quizzler-app-start/ui.py
@@ -1,2 +1,35 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
+
+class UI():
+    def __init__(self):
+        # super().__init__
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(bg=THEME_COLOR, padx=50, pady=50)
+        # self.window.minsize(400,600)
+        
+        self.canvas = Canvas(width=300,height=250)
+        self.canvas.grid(column=0, row=1, columnspan=2, pady=50) #this padY is how we create space between the canvas and buttons
+        
+        self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR, font=("Arial", 20, "normal"))
+        self.score_label.grid(column=1,row=0)
+
+        self.question_text = self.canvas.create_text(
+            150,125,
+            text="Question",
+            fill=THEME_COLOR,
+            font=("Arial", 30, "bold"))
+        # self.canvas.itemconfig(self.question_text, )
+    
+        right_button_img = PhotoImage(file="images/true.png")
+        wrong_button_img = PhotoImage(file="images/false.png")
+        self.right = Button(image=right_button_img, highlightthickness=0)
+        self.right.grid(column=0, row=2)
+        self.wrong = Button(image=wrong_button_img, highlightthickness=0)
+        self.wrong.grid(column=1, row=2)
+        
+        self.window.mainloop()
+
 
