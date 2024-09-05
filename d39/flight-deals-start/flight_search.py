5ba717269baa9c34c190570f1c93035b29047b4f
levietha92 <ha.lv@joonsolutions.com>
2024-09-05 16:35:19 +0700

    500 error gone, barebone finished

--- d39/flight-deals-start/flight_search.py
+++ d39/flight-deals-start/flight_search.py
@@ -36,6 +36,7 @@ class FlightSearch:
         print(f"ClientID:{self.client_id}, client secret: {self.client_secret}")
         
     def get_access_token(self):
+        print("Getting access token")
         data = {
             "grant_type": "client_credentials",
             "client_id": self.client_id,
@@ -48,6 +49,7 @@ class FlightSearch:
         return response.json()["access_token"]
 
     def get_flights(self,origin: str,destination: str,adults=1,departure_date="2024-08-01"):# departure_date=dt.datetime.today().strftime("%Y-%m-%d")
+        print(f"Getting Flight info for {origin}>{destination}, {departure_date}")
         endpoint = "v2/shopping/flight-offers"
         url = f"{self.host_domain}/{endpoint}"
         self.parameters = {
@@ -56,22 +58,26 @@ class FlightSearch:
             "departureDate": departure_date,
             "adults": adults
         }
-        response = requests.get(url=url, params=self.parameters, headers=self.headers)
-        response.raise_for_status()
-        return response.json()
+        try:
+            response = requests.get(url=url, params=self.parameters, headers=self.headers)
+            return response.json()
+        except KeyError as e:
+            return e
     
     def get_iata_code(self, city_name:str):
+        print(f"Getting IATA Code for {city_name}")
         iata_endpoint = f"{self.host_domain}/v1/reference-data/locations/cities?keyword={city_name}&include=AIRPORTS"
         try:
             response = requests.get(url=iata_endpoint,headers=self.headers)
+            return response.json()['data'][0]['iataCode']
         except IndexError:
             print(f"IndexError: No airport code found for {city_name}.")
             return "N/A"
         except KeyError:
             print(f"KeyError: No airport code found for {city_name}.")
             return "Not Found"
-        response.raise_for_status()
-        # return response.json()
+        # response.raise_for_status()
+        
 
 
 ### TEST CODE
@@ -80,12 +86,27 @@ flight.access_token
 
 city_name = "ATL"
 response = requests.get(url=f"https://test.api.amadeus.com/v1/reference-data/locations/cities?keyword={city_name}",
-    headers=flight.headers,
-    # params = {
-    #         "grant_type": "client_credentials",
-    #         "client_id": os.getenv("API_KEY_FLIGHT"),
-    #         "client_secret": os.getenv("API_SECRET_FLIGHT")
-    #     }
+    headers=flight.headers
 )        
 response.raise_for_status()
 response.json()
+
+origin = "HAN"
+destination = "PAR"
+departure_date = "2024-10-01"
+headers = {"Authorization": f"Bearer {flight.access_token}"}
+adults=1
+
+print(f"Getting Flight info for {origin}>{destination}, {departure_date}")
+url = "https://test.api.amadeus.com/v2/shopping/flight-offers"
+parameters = {
+    "originLocationCode": origin,
+    "destinationLocationCode": destination,
+    "departureDate": departure_date,
+    "adults": adults
+}
+response = requests.get(url=url, params=parameters, headers=headers)
+response.raise_for_status()
+response.json()
+
+response.json()['data'][0]['price']['total']
--- d39/flight-deals-start/main.py
+++ d39/flight-deals-start/main.py
@@ -7,16 +7,29 @@ from pprint import pprint
 
 data = DataManager()
 sheet_data = data.get_sheet_data()
-print(sheet_data)
-
-
 flight_search = FlightSearch()
-flight_search.get_iata_code("HANOI")
 
+#writing IATACode into the sheet
 for i in range(0, len(sheet_data)):
     if len(sheet_data[i]['iataCode'])==0:
         new_record = {
             'price': {
-                'iataCode': flight_search.get_iata_code()[i]
+                'iataCode': flight_search.get_iata_code(sheet_data[i]['city'])
             }}
-        put_response = requests.put(url=f"{endpoint}/{i+2}",json=new_record)
+        put_response = requests.put(url=f"{data.endpoint}/{i+2}",json=new_record)
+        destination_code = put_response.json()['price']['iataCode']
+        print(destination_code)
+
+        # Getting flight info
+        flight_response = flight_search.get_flights(
+            origin="HAN",
+            destination=destination_code,
+            departure_date="2024-10-01",
+            adults=1
+            )
+
+        flight_response_price = float(flight_response['data'][0]['price']['total'])
+        if flight_response_price < sheet_data[i]['lowestPrice']:
+            print("send Twilio")
+        else:
+            print("Price not good")
