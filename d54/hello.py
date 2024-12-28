71075af774ec69978ae56a266240cb2148e17689
levietha92 <levietha.ft@gmail.com>
2024-12-28 16:41:59 +0700

    d54 flask variable

--- d54/hello.py
+++ d54/hello.py
@@ -6,7 +6,13 @@ app = Flask(__name__)
 def hello_world():
     return "Hello, World!"
 
-@app.route("/bye") #homepage/
-def hello_world():
+@app.route("/bye") #homepage/bye
+def bye():
     return "BYEEEE"
 
+@app.route("/user/<name>")
+def greet(name):
+    return f"Hola {name} boo"
+
+if __name__=="__main__":
+    app.run(debug=True)
