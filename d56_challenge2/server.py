4ba59786382860995f5db5ad87f8004b3e98666d
levietha92 <levietha.ft@gmail.com>
2024-12-29 12:59:36 +0700

    d56 capstone done

---
+++ d56_challenge2/server.py
@@ -0,0 +1,12 @@
+#Patrick Batman Namecard project
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+  return render_template("index.html")
+
+
+if __name__=='__main__':
+  app.run(debug=True)
