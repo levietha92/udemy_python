97fe4bad5e8085eb73d3f1e26fb8230f7ad79515
levietha92 <levietha.ft@gmail.com>
2024-12-29 11:33:19 +0700

    d55 solved

--- d55/exercise.py
+++ d55/exercise.py
@@ -1,15 +1,14 @@
 # TODO: Create the logging_decorator() function 👇
-
 def logging_decorator(function):
     def wrapper(*args):
         result = function(*args)  # Unpack args and store the result
-        print(f"You called {function.__name__}, it returned {result}")
+        print(f"You called {function.__name__}{(args)}\nIt returned: {result}")
     return wrapper  # Return the wrapper function
 
-# TODO: Use the decorator 👇
 
+# TODO: Use the decorator 👇
 @logging_decorator
 def a_function(*args):
     return sum(args)
     
-a_function(1, 2, 3)  # This will print "You called a_function, it returned 6"
+a_function(1,2,3)
