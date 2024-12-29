03ddc4e73362e38da0e74f79e245922950c89c08
levietha92 <levietha.ft@gmail.com>
2024-12-29 13:37:11 +0700

    d57 guess solved

--- d57/main.py
+++ d57/main.py
@@ -1,17 +1,29 @@
 from flask import Flask, render_template
 import random
 import datetime
+import requests
 
 app = Flask(__name__)
 random_number = random.randint(0,100)
+current_year = datetime.date.today().year
 
 @app.route('/')
 def home():
     return render_template('index.html', 
                            num=random_number,
-                           current_year=datetime.date.today().year
+                           current_year=current_year
                            )
-
+@app.route('/<name>')
+def guess(name):
+    gender_url=f'https://api.genderize.io?name={name}'
+    age_url=f'https://api.agify.io?name={name}'
+    
+    return render_template('guess.html',
+                           name=name,
+                           request_gender= requests.get(gender_url),
+                           request_age = requests.get(age_url)
+                           )
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
