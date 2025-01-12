96313af628d7494aa5895815c841f7236bef6075
levietha92 <levietha.ft@gmail.com>
2025-01-12 17:11:30 +0700

    d57 blogpost

--- d57/main.py
+++ d57/main.py
@@ -23,7 +23,13 @@ def guess(name):
                            request_gender= requests.get(gender_url),
                            request_age = requests.get(age_url)
                            )
-    
+   
+@app.route('/blog')
+def blog():
+    blog_url='https://api.npoint.io/c790b4d5cab58020d391'
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template('blog.html', posts=all_posts) 
 
 if __name__ == "__main__":
     app.run(debug=True)
