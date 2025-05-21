d3ac842a63094a835f4e2d881fe81048807c6dae
levietha92 <levietha.ft@gmail.com>
2025-05-21 16:34:12 +0700

    d57

--- d57/main.py
+++ d57/main.py
@@ -13,7 +13,7 @@ def home():
                            num=random_number,
                            current_year=current_year
                            )
-@app.route('/<name>')
+@app.route('/name/<name>')
 def guess(name):
     gender_url=f'https://api.genderize.io?name={name}'
     age_url=f'https://api.agify.io?name={name}'
@@ -24,12 +24,13 @@ def guess(name):
                            request_age = requests.get(age_url)
                            )
    
-@app.route('/blog')
-def blog():
+@app.route('/blog/<number>')
+def get_blog(number):
+    print(number)
     blog_url='https://api.npoint.io/c790b4d5cab58020d391'
     response = requests.get(blog_url)
     all_posts = response.json()
-    return render_template('blog.html', posts=all_posts) 
+    return render_template('blog.html', posts=all_posts, number=number) 
 
 if __name__ == "__main__":
     app.run(debug=True)
